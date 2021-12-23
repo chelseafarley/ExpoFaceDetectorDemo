218e67f38303d7d500c32df91c6759753cb4c653
Chelsea Farley <chelseafarley91@gmail.com>
2021-12-23 14:53:16 +1300

    Face Detector Demo

--- App.js
+++ App.js
@@ -1,20 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Camera } from 'expo-camera';
+import * as FaceDetector from 'expo-face-detector';
 
 export default function App() {
+  const [hasPermission, setHasPermission] = React.useState();
+  const [faceData, setFaceData] = React.useState([]);
+
+  React.useEffect(() => {
+    (async () => {
+      const {status} = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  function getFaceDataView() {
+    if (faceData.length === 0) {
+      return (
+        <View style={styles.faces}>
+          <Text style={styles.faceDesc}>No faces :(</Text>
+        </View>
+      );
+    } else {
+      return faceData.map((face, index) => {
+        const eyesShut = face.rightEyeOpenProbability < 0.4 && face.leftEyeOpenProbability < 0.4;
+        const winking = !eyesShut && (face.rightEyeOpenProbability < 0.4 || face.leftEyeOpenProbability < 0.4);
+        const smiling = face.smilingProbability > 0.7;
+        return (
+          <View style={styles.faces} key={index}>
+            <Text style={styles.faceDesc}>Eyes Shut: {eyesShut.toString()}</Text>
+            <Text style={styles.faceDesc}>Winking: {winking.toString()}</Text>
+            <Text style={styles.faceDesc}>Smiling: {smiling.toString()}</Text>
+          </View>
+        );
+      });
+    }
+  }
+
+  const handleFacesDetected = ({ faces }) => {
+    setFaceData(faces);
+    console.log(faces);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Camera 
+      type={Camera.Constants.Type.front}
+      style={styles.camera}
+      onFacesDetected={handleFacesDetected}
+      faceDetectorSettings={{
+        mode: FaceDetector.FaceDetectorMode.fast,
+        detectLandmarks: FaceDetector.FaceDetectorLandmarks.none,
+        runClassifications: FaceDetector.FaceDetectorClassifications.none,
+        minDetectionInterval: 100,
+        tracking: true
+      }}>
+      {getFaceDataView()}
+    </Camera>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  camera: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  faces: {
+    backgroundColor: '#ffffff',
+    alignSelf: 'stretch',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 16
+  },
+  faceDesc: {
+    fontSize: 20
+  }
 });
